c03ad7e16437d408e55daede07cf48291c8af071
LipeGouveia <39633068+LipeGouveia@users.noreply.github.com>
2018-05-25 17:03:26 -0300

    Update Teste_Ys.java

--- src/teste_ys/Teste_Ys.java
+++ src/teste_ys/Teste_Ys.java
@@ -5,7 +5,7 @@ public class Teste_Ys {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
+		System.out.println("O loko meu");
 	}
 
 }
